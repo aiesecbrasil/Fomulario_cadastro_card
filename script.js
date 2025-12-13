89c382757c8a63b3cb9e133231395faf8ff98124
Caio-Marinho <kaigabril12@gmail.com>
2025-12-13 17:17:45 -0300

    ajustando texto do modal

--- script.js
+++ script.js
@@ -831,8 +831,8 @@ document.getElementById('meuForm').addEventListener('submit', function (e) {
 
         let dados = `Nome: ${nome}<br>
         Sobrenome: ${sobrenome}<br>
-        Emails: ${emails.map(email => `${email.email} (${email.tipoTraduzido})`).join('\t\t<br>')}<br>
-        Telefones: ${telefones.map(telefone => `${telefone.numero} (${telefone.tipoTraduzido})`).join('\t\t<br>')}<br>
+        Emails: ${emails.map(email => `${email.email} (${email.tipoTraduzido})`).join('<br>\t\t')}<br>
+        Telefones: ${telefones.map(telefone => `${telefone.numero} (${telefone.tipoTraduzido})`).join('<br>\t\t')}<br>
         Data de Nascimento: ${inputVisivel.value}<br>`;
 
         // Adiciona só se o campo existir
